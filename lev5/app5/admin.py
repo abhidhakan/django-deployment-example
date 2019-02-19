15fac4b484ad3e88bcc95b9670c5b688ffd82047
Dhakan's PC <abhi@gmail.com>
2019-02-19 22:21:43 +0530

    'first'

---
+++ lev5/app5/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from app5.models import UserProfileInfo, User
+# Register your models here.
+admin.site.register(UserProfileInfo)
---
+++ lev5/app5/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from app5 import views
+
+# SET THE NAMESPACE!
+app_name = 'app5'
+
+
+urlpatterns=[
+    url(r'^register/$',views.register,name='register'),
+    url(r'^user_login/$',views.user_login,name='user_login'),
+]
